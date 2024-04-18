9f975aa6d7f1660d5c0b1b2cf2f3ddc5bdc06abd
arup-sarkar-24 <1arupsarkar112@gmail.com>
2024-04-18 12:02:36 +0530

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {FaBars, FaTimes} from "react-icons/fa";
+function NavBar() {
+  return (
+    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
+        <div>
+            <h1 >Arup</h1>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar;
